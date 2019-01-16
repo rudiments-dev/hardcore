064eb701babfb2c28e077b7818d47313bf109e24
gennady <gennady.lebedev@gmail.com>
2019-01-16 21:01:01 +0300

    improve ID

--- src/test/scala/work/unformed/hardcore/spec/IDSpec.scala
+++ src/test/scala/work/unformed/hardcore/spec/IDSpec.scala
@@ -1,7 +1,7 @@
 package work.unformed.hardcore.spec
 
 import org.scalatest.{Matchers, WordSpec}
-import work.unformed.hardcore.dsl.{ID, ID1}
+import work.unformed.hardcore.dsl.{ID, ID1, Meta}
 
 case class Example(
   id: Long,
@@ -10,9 +10,29 @@ case class Example(
 )
 
 class IDSpec extends WordSpec with Matchers {
+  val sample = Example(42, "sample")
+
   "explicit ID from instance field" in {
-    val sample = Example(42, "sample")
-    val id = ID[Example, Long](sample.id)
+    val id: ID[Example] = ID(sample.id)
     id should be (ID1[Example, Long](42))
+  }
+
+  "explicit meta generates ID from instance" in {
+    val meta = new Meta[Example] {
+      override def identify(value: Example): ID[Example] = ID(value.id)
+    }
+    meta.identify(sample) should be (ID1[Example, Long](42))
+  }
+
+  "construct meta from function generate ID from instance" in {
+    val meta: Meta[Example] = Meta(value => ID(value.id))
+    meta.identify(sample) should be (ID1[Example, Long](42))
+  }
+
+  "construct meta from function, implicit ID from instance" in {
+    import work.unformed.hardcore.dsl.ID._
+
+    implicit val meta: Meta[Example] = Meta(value => ID(value.id))
+    sample.identify should be (ID1[Example, Long](42))
   }
 }
