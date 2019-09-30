ba67359e247bf10daa815574f6f16e0e5082e8d2
gennady <gennady.lebedev@gmail.com>
2019-09-30 22:27:23 +0300

    implement read-only http port

---
+++ implementation/src/main/scala/dev/rudiments/hardcore/data/ReadOnlyHttpPort.scala
@@ -0,0 +1,38 @@
+package dev.rudiments.hardcore.data
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.{Directive1, Route, StandardRoute}
+import dev.rudiments.hardcore.Port
+import dev.rudiments.hardcore.http.CirceSupport._
+import dev.rudiments.hardcore.http.Router
+import dev.rudiments.hardcore.types.{DTO, ID, Type}
+import io.circe.{Decoder, Encoder}
+
+class ReadOnlyHttpPort[T <: DTO : Type : Encoder : Decoder](
+  prefix: String,
+  idDirective: Directive1[ID[T]],
+  override val f: DataSkill[T]
+) extends Port[DataCommand[T], DataEvent[T]] with Router {
+
+  override val routes: Route = pathPrefix(prefix) {
+    get {
+      pathEndOrSingleSlash {
+        responseWith(f(FindAll[T]))
+      } ~ idDirective { id =>
+        pathEndOrSingleSlash {
+          responseWith(f(Find(id)))
+        }
+      }
+    }
+  }
+
+  def responseWith(event: DataEvent[T]): StandardRoute = event match {
+    case Found(_, value) =>   complete(StatusCodes.OK, value)
+    case FoundAll(values) =>  complete(StatusCodes.OK, values)
+    case NotFound(_) =>       complete(StatusCodes.NotFound)
+    case _: Error =>          complete(StatusCodes.InternalServerError)
+  }
+}
+
+
