7de00a2f87fc3e1d8eb9a6a4b3ed66a97fe88a06
gennady <gennady.lebedev@gmail.com>
2019-10-01 09:05:38 +0300

    implement almost all types of H2

--- db-registry/src/test/scala/dev/rudiments/db/registry/H2AdapterSpec.scala
+++ db-registry/src/test/scala/dev/rudiments/db/registry/H2AdapterSpec.scala
@@ -7,6 +7,7 @@ import org.scalatest.junit.JUnitRunner
 import scalikejdbc._
 
 import collection.JavaConverters._
+import SizeMultipliers.N
 
 @RunWith(classOf[JUnitRunner])
 class H2AdapterSpec extends WordSpec with Matchers {
@@ -36,10 +37,26 @@ class H2AdapterSpec extends WordSpec with Matchers {
          |)""".stripMargin.execute().apply()
 
     sql"""CREATE TABLE example (
-         |    id IDENTITY PRIMARY KEY,
-         |    sample_id BIGINT NOT NULL,
-         |    comment CLOB,
-         |    FOREIGN KEY (sample_id) REFERENCES sample (id)
+         |    id                IDENTITY PRIMARY KEY,
+         |    int_column        INT,
+         |    bool_column       BOOLEAN,
+         |    tinyint_column    TINYINT,
+         |    smallint_column   SMALLINT,
+         |    bigint_column     BIGINT,
+         |    decimal_column    DECIMAL(12, 3),
+         |    double_column     DOUBLE(12),
+         |    real_column       REAL(12),
+         |    time_column       TIME,
+         |    date_column       DATE,
+         |    timestamp_column  TIMESTAMP,
+         |    binary_column     BINARY(5),
+         |    varchar_column    VARCHAR(67),
+         |    char_column       CHAR(89),
+         |    blob_column       BLOB(10),
+         |    clob_column       CLOB(10),
+         |    uuid_column       UUID,
+         |    array_column      ARRAY,
+         |    enum_column       ENUM('RED', 'GREEN', 'BLUE')
          |)""".stripMargin.execute().apply()
 
     adapter(DiscoverSchema("hello")) should be (SchemaFound("hello", Set("SAMPLE", "EXAMPLE")))
@@ -50,7 +67,32 @@ class H2AdapterSpec extends WordSpec with Matchers {
       TableFound("sample", Seq(
         Column("ID", ColumnTypes.BIGINT, false, true, true),
         Column("NAME", ColumnTypes.VARCHAR(255), false, false, false),
-        Column("COMMENT", ColumnTypes.CLOB(2147483647, SizeMultipliers.N), true, false, false),
+        Column("COMMENT", ColumnTypes.CLOB(2147483647, N), true, false, false),
+      ))
+    )
+
+    adapter(DiscoverTable("example", "hello")) should be (
+      TableFound("example", Seq(
+        Column("ID",                ColumnTypes.BIGINT,               false, true,  true),
+        Column("INT_COLUMN",        ColumnTypes.INT,                  true,  false, false),
+        Column("BOOL_COLUMN",       ColumnTypes.BOOLEAN,              true,  false, false),
+        Column("TINYINT_COLUMN",    ColumnTypes.TINYINT,              true,  false, false),
+        Column("SMALLINT_COLUMN",   ColumnTypes.SMALLINT,             true,  false, false),
+        Column("BIGINT_COLUMN",     ColumnTypes.BIGINT,               true,  false, false),
+        Column("DECIMAL_COLUMN",    ColumnTypes.DECIMAL(12),          true,  false, false),
+        Column("DOUBLE_COLUMN",     ColumnTypes.DOUBLE(17),           true,  false, false),
+        Column("REAL_COLUMN",       ColumnTypes.REAL(7),              true,  false, false),
+        Column("TIME_COLUMN",       ColumnTypes.TIME(8, true),        true,  false, false),
+        Column("DATE_COLUMN",       ColumnTypes.DATE(10),             true,  false, false),
+        Column("TIMESTAMP_COLUMN",  ColumnTypes.TIMESTAMP(26, true),  true,  false, false),
+        Column("BINARY_COLUMN",     ColumnTypes.BINARY(5),            true,  false, false),
+        Column("VARCHAR_COLUMN",    ColumnTypes.VARCHAR(67),          true,  false, false),
+        Column("CHAR_COLUMN",       ColumnTypes.CHAR(89),             true,  false, false),
+        Column("BLOB_COLUMN",       ColumnTypes.BLOB(10, N),          true,  false, false),
+        Column("CLOB_COLUMN",       ColumnTypes.CLOB(10, N),          true,  false, false),
+        Column("UUID_COLUMN",       ColumnTypes.UUID(16),             true,  false, false),
+        Column("ARRAY_COLUMN",      ColumnTypes.ARRAY(2147483647),    true,  false, false),
+        Column("ENUM_COLUMN",       ColumnTypes.ENUM(5),              true,  false, false)
       ))
     )
   }
