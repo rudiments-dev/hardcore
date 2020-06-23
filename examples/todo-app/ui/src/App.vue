8acb01efd8707a2c62f4db524e41f6e090387d22
Iskander Ignatev <iskander.ignatev@gmail.com>
2020-06-24 00:24:21 +0300

    GH-25 dockerize todo-app backend and frontend, add gradle plugin for shadowjaring the todo-app instance

---
+++ examples/todo-app/ui/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app" class="w-full h-screen p-2">
+    <Todo v-bind:todos="todos" />
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  },
+  data: function() {
+    return {
+      todos: {},
+      errors: []
+    }
+  },
+    created() {
+    axios.get(process.env.VUE_APP_BACKEND + "/todo")
+    .then(response => {
+      this.todos = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  background: #406CC3
+}
+</style>
