2203a490ca4e14f2d48d0c11d8edab3ec78695d3
gennady <gennady.lebedev@gmail.com>
2020-06-25 08:52:42 +0300

    drop complicated logic of caching commands and events
    #32

--- implementation/src/main/scala/dev/rudiments/hardcore/flow/Controlled.scala
+++ implementation/src/main/scala/dev/rudiments/hardcore/flow/Controlled.scala
@@ -5,30 +5,9 @@ import dev.rudiments.hardcore.{Command, Event, Result, Skill}
 
 class Controlled[E <: Event](skill: Skill[E])(implicit flow: ControlFlow) extends Skill[E] {
 
-  override def isDefinedAt(cmd: Command): Boolean = cmd match {
-    case c: Command if skill.isDefinedAt(c) => true
-    case _: ControlCommand => false //for future use
-    case other => false
-  }
+  override def isDefinedAt(cmd: Command): Boolean = skill.isDefinedAt(cmd)
 
-  override def apply(cmd: Command): Result[E] = cmd match {
-    case c: AlwaysDo => execute(c)
-    case c: BulkRead => execute(c)
-    case c: SideEffect => execute(c) //TODO think!
-    case c: CacheSingle =>
-      flow.cachedContext.get(c.key) match {
-        case Some((command, nested: Command)) if command == cmd => this(nested)
-        case Some((command, event: Event)) if command == cmd => event.toEither
-        case Some((command, other)) if command != cmd => execute(cmd)
-        case None => execute(c)
-      }
-    case c: Command =>
-      flow.lastMessage(c) match { //TODO wrong?
-        case Some(command: Command) => this(command)
-        case Some(evt: Event) => evt.toEither
-        case None => execute(cmd)
-      }
-  }
+  override def apply(cmd: Command): Result[E] = execute(cmd)
 
   private def execute(cmd: Command): Result[E] = {
     cmd match {
