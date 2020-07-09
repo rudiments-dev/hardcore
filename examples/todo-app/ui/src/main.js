45e1726d1153e7896814d79af8918d8b6745f60f
Iskander Ignatev <iskander.ignatev@gmail.com>
2020-07-09 16:57:32 +0300

    GH-76 WIP squash me, add vuex, fetch todos and put them to the vuex store

--- examples/todo-app/ui/src/main.js
+++ examples/todo-app/ui/src/main.js
@@ -1,9 +1,25 @@
-import Vue from 'vue'
 import App from './App.vue'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import todos from './modules/todos';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
+  modules: {
+    todos,
+  },
+});
+
 import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  store,
 }).$mount('#app')
