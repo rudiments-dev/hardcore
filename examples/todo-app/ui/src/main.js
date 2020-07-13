7539fd174d19305acf8df51f89820a548142feb6
Iskander Ignatev <iskander.ignatev@gmail.com>
2020-07-13 07:31:16 +0300

    GH-76 WIP squash me, populate store with getter

--- examples/todo-app/ui/src/main.js
+++ examples/todo-app/ui/src/main.js
@@ -1,11 +1,12 @@
 import App from './App.vue'
 import Vue from 'vue'
+import Vuex from 'vuex'
 import store from './store/store.js'
-
-
 import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuex);
 
 new Vue({
   render: h => h(App),
