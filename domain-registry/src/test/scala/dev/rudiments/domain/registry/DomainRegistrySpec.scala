aa50cefb4afe66af171b19b8faef620ef191d6c7
Gennady Lebedev <gennady.lebedev@gmail.com>
2020-09-02 01:48:11 +0300

    add contract and examples for Domain Registry

--- domain-registry/src/test/scala/dev/rudiments/domain/registry/DomainRegistrySpec.scala
+++ domain-registry/src/test/scala/dev/rudiments/domain/registry/DomainRegistrySpec.scala
@@ -3,7 +3,6 @@ package dev.rudiments.domain.registry
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.typesafe.config.ConfigFactory
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import dev.rudiments.another._
 import io.circe.{Decoder, Encoder}
@@ -14,7 +13,6 @@ import org.scalatest.{Matchers, WordSpec}
 
 @RunWith(classOf[JUnitRunner])
 class DomainRegistrySpec extends WordSpec with Matchers with ScalatestRouteTest with FailFastCirceSupport {
-  private val config = ConfigFactory.load()
   private val skill = new DomainSkill()
 
   private implicit val en: Encoder[Instance] = new ThingEncoder(skill.domain).abstractInstanceEncoder("Thing")
@@ -27,17 +25,17 @@ class DomainRegistrySpec extends WordSpec with Matchers with ScalatestRouteTest 
     skill
   )(en, de)
 
-  private val routes = Route.seal(new RootRouter(config, http).routes)
+  private val routes = Route.seal(http.routes)
 
   "get all Thing in Domain" in {
-    Get("/api/domain") ~> routes ~> check {
+    Get("/domain") ~> routes ~> check {
       response.status should be (StatusCodes.OK)
       responseAs[Seq[Instance]].size should be (26)
     }
   }
 
   "get Thing" in {
-    Get("/api/domain/Thing") ~> routes ~> check {
+    Get("/domain/Thing") ~> routes ~> check {
       response.status should be (StatusCodes.OK)
       responseAs[Instance] should be (Instance(
         skill.domain.find[Spec]("Abstract"),
