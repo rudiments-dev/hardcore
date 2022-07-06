db10bf2a36cf3fb8275c3c000cf391f60dfa9474
gennady <gennady.lebedev@gmail.com>
2022-07-06 10:28:27 +0300

    eliminate state of FileAgent, memory extracted outside

--- implementation/src/main/scala/dev/rudiments/hardcore/file/FileAgent.scala
+++ implementation/src/main/scala/dev/rudiments/hardcore/file/FileAgent.scala
@@ -4,29 +4,24 @@ import dev.rudiments.hardcore._
 
 import java.io.{File => JavaFile}
 import java.nio.file.{Files, Paths}
-import scala.collection.mutable
 import scala.io.Source
 import scala.util.Using
 
 class FileAgent(absolutePath: String) {
-  val data: Memory = new Memory()
-  val files: mutable.Map[Location, File] = mutable.Map.empty
-
   def read(where: Location): Out = where match {
-    case Root => readFile(absolutePath, Folder)
-    case id: ID => readFile(absolutePath + "/" + id.key.toString, files(id))
-    case path: Path => readFile(absolutePath + "/" + path.toString, files(path))
+    case Root => readFile(absolutePath)
+    case id: ID => readFile(absolutePath + "/" + id.key.toString)
+    case path: Path => readFile(absolutePath + "/" + path.toString)
   }
 
-  def load(where: Location): Out = deltaTx(new Tx(data), where)
+  def load(where: Location, into: Memory): Out = deltaTx(new Tx(into), where)
 
-  def deltaTx(tx: Tx, where: Location): Out = (read(where), data.read(where)) match {
+  def deltaTx(tx: Tx, where: Location): Out = (read(where), tx.read(where)) match {
     case (Readen(file), NotExist) =>
       file match {
         case Data(Folder.typeOf, fs: Map[ID, File]) =>
           tx.remember(where, Created(file))
           fs.foreach { case (id, f) =>
-            files += where / id -> f
             deltaTx(tx, where / id)
           }
           tx.>>
@@ -46,38 +41,29 @@ class FileAgent(absolutePath: String) {
     case (that, other) => Conflict(that, other)
   }
 
-  def readFile(path: String, file: File): Out = {
+  def readFile(path: String): Out = {
     val f = new JavaFile(path)
     if(!f.exists()) {
       NotExist
     } else {
-      file match {
-        case Folder =>
-          try {
-            if(!f.isDirectory) throw new IllegalArgumentException(s"File $path should be directory")
-
-            Readen(Data(Folder.typeOf,
-              f.listFiles().toSeq.map {
-                case f: JavaFile if f.isDirectory => ID(f.getName) -> Folder
-                case f: JavaFile if f.isFile && TextFile.isTextFile(f.getName) => ID(f.getName) -> TextFile
-                case f: JavaFile if f.isFile => ID(f.getName) -> UnknownFile
-              }.toMap
-            ))
-          } catch {
-            case e: Exception => FileError(e.getMessage)
-          }
-
-        case TextFile =>
+      if(f.isDirectory) {
+        Readen(Data(Folder.typeOf,
+          f.listFiles().toSeq.map {
+            case f: JavaFile if f.isDirectory => ID(f.getName) -> Folder
+            case f: JavaFile if f.isFile && TextFile.isTextFile(f.getName) => ID(f.getName) -> TextFile
+            case f: JavaFile if f.isFile => ID(f.getName) -> UnknownFile
+          }.toMap
+        ))
+      } else if (f.isFile) {
+        if(TextFile.isTextFile(f.getName)) {
           Using(Source.fromFile(path)) { f =>
             Readen(Data(TextFile.typeOf, f.getLines().toSeq))
           }.getOrElse(FileError(s"Failed to read $path"))
-
-        case UnknownFile =>
-          try {
-            Readen(Data(Binary, Files.readAllBytes(Paths.get(path))))
-          } catch {
-            case e: Exception => FileError(e.getMessage)
-          }
+        } else {
+          Readen(Data(Binary, Files.readAllBytes(Paths.get(path))))
+        }
+      } else {
+        FileError(s"Unknown file ${f.getAbsolutePath}")
       }
     }
   }
--- implementation/src/test/scala/test/dev/rudiments/hardcore/file/FileSpec.scala
+++ implementation/src/test/scala/test/dev/rudiments/hardcore/file/FileSpec.scala
@@ -11,6 +11,7 @@ import org.scalatestplus.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class FileSpec extends AnyWordSpec with Matchers {
   private val fileAgent = new FileAgent("src/test/resources/file-test")
+  private val mem: Memory = new Memory
 
   "can read file in Agent" in {
     fileAgent.read(Root) should be (
@@ -28,7 +29,7 @@ class FileSpec extends AnyWordSpec with Matchers {
   }
 
   "can prepare commit via Agent" in {
-    fileAgent.load(Root) should be(Prepared(Commit(
+    fileAgent.load(Root, mem) should be(Prepared(Commit(
       Map(
         Root -> Created(Data(Folder.typeOf, Map(
           ID("folder1") -> Folder,
@@ -46,13 +47,13 @@ class FileSpec extends AnyWordSpec with Matchers {
     )))
   }
 
-  "can save prepared inside Agent" in {
-    fileAgent.load(Root) match {
-      case Prepared(cmt) => fileAgent.data << cmt should be (Committed(cmt))
+  "can save prepared into Memory" in {
+    fileAgent.load(Root, mem) match {
+      case Prepared(cmt) => mem << cmt should be (Committed(cmt))
       case _ => fail("Unexpected result of load")
     }
 
-    fileAgent.data << Find(All) should be (Found(All, Map(
+    mem << Find(All) should be (Found(All, Map(
       Root -> Data(Folder.typeOf, Map(
         ID("folder1") -> Folder,
         ID("24.bin") -> UnknownFile,
