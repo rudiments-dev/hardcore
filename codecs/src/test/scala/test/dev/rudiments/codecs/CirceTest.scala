555370bf150a7da0e9dee93ac37e2e2928f3cdd6
gennady <gennady.lebedev@gmail.com>
2023-05-07 15:23:06 +0600

    use milestone circe

---
+++ codecs/src/test/scala/test/dev/rudiments/codecs/CirceTest.scala
@@ -0,0 +1,35 @@
+package test.dev.rudiments.codecs
+
+import io.circe.{Codec, Json}
+import io.circe.generic.semiauto.*
+import org.junit.runner.RunWith
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class CirceTest extends AnyWordSpec with Matchers {
+  case class Sample(
+    a: Int,
+    b: String,
+    c: Seq[String]
+  )
+
+  implicit val codec: Codec[Sample] = Codec.from(deriveDecoder[Sample], deriveEncoder[Sample])
+  private val sample = Sample(42, "the answer", Seq("to", "life", "the", "universe", "and", "everything"))
+
+  "can encode custom class" in {
+    codec(sample) should be (Json.obj(
+      "a" -> Json.fromInt(42),
+      "b" -> Json.fromString("the answer"),
+      "c" -> Json.arr(
+        Json.fromString("to"),
+        Json.fromString("life"),
+        Json.fromString("the"),
+        Json.fromString("universe"),
+        Json.fromString("and"),
+        Json.fromString("everything")
+      )
+    ))
+  }
+}
--- core/src/main/scala/dev/rudiments/utils/ZLib.scala
+++ core/src/main/scala/dev/rudiments/utils/ZLib.scala
@@ -4,9 +4,9 @@ import java.io.ByteArrayOutputStream
 import java.util.zip.{Deflater, Inflater}
 
 object ZLib {
-  val BUFFER_SIZE = 4096
+  val DEFAULT_BUFFER_SIZE = 4096
 
-  def pack(data: Array[Byte]): Array[Byte] = {
+  def pack(data: Array[Byte], size: Int = DEFAULT_BUFFER_SIZE): Array[Byte] = {
     val deflater = new Deflater()
     deflater.setLevel(Deflater.DEFAULT_COMPRESSION)
     deflater.setInput(data)
@@ -14,7 +14,7 @@ object ZLib {
     val outputStream = new ByteArrayOutputStream(data.length)
     try {
       deflater.finish()
-      val buffer = new Array[Byte](BUFFER_SIZE)
+      val buffer = new Array[Byte](size)
       while (!deflater.finished) {
         val count = deflater.deflate(buffer)
         outputStream.write(buffer, 0, count)
@@ -24,13 +24,13 @@ object ZLib {
       if (outputStream != null) outputStream.close()
   }
 
-  def unpack(data: Array[Byte]): Array[Byte] = {
+  def unpack(data: Array[Byte], size: Int = DEFAULT_BUFFER_SIZE): Array[Byte] = {
     val inflater = new Inflater()
     inflater.setInput(data)
 
     val outputStream = new ByteArrayOutputStream(data.length)
     try {
-      val buffer = new Array[Byte](BUFFER_SIZE)
+      val buffer = new Array[Byte](size)
       while (!inflater.finished) {
         val count = inflater.inflate(buffer)
         outputStream.write(buffer, 0, count)
